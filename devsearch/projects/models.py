d092ba88049f92a63b68432bb515245b3f3974c3
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-12-06 07:42:06 +0300

    updating models

--- devsearch/projects/models.py
+++ devsearch/projects/models.py
@@ -9,3 +9,4 @@ class Project(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
+    
