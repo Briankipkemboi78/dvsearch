89a0b83fa57b647aa0ac853c9f71b9110c28bb4b
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-12-07 16:53:11 +0300

    implementing addin a project to the database

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,34 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Project 
+from .forms import ProjectForm
 
 
-projectsList = [
-    {
-        'id': '1',
-        'title': 'Ecommerce Website',
-        'description': 'Fully functional ecommerce website'
-    },
-    {
-        'id': '2',
-        'title': 'Portfolio Website',
-        'description': 'Fully functional portfolio website'
-    },
-    {
-        'id': '3',
-        'title': 'Social Network',
-        'description': 'This is a work in project. Watch this space'
-    },
-]
 
 def projects(request):
-    page = 'Projects'
-    number = 11
-    context = {'page':page, 'number': number, 'projects':projectsList}
+    projects = Project.objects.all()
+    context = {'projects':projects}
     return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
-    projectObj = None
-    for i in projectsList:
-        if i['id'] == pk:
-            projectObj = i
-    return render(request, 'projects/single-project.html', {'project': projectObj} )
+    projectObj = Project.objects.get(id=pk)
+    return render(request, 'projects/single-project.html', {'project': projectObj} )\
+    
+def createProject(request):
+    form = ProjectForm()
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    
+    context = {'form': form}
+    return render(request, "projects/project_form.html", context )
+
+
+def updateProject(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=Project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    
+    context = {'form': form}
+    return render(request, "projects/project_form.html", context )
