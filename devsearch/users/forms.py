a19319f14719eb03a787faa6da69c7e376c427b3
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-12-30 21:59:14 +0300

    Formatting the forms

--- devsearch/users/forms.py
+++ devsearch/users/forms.py
@@ -9,3 +9,11 @@ class CustomUserCreationForm(UserCreationForm):
         labels = {
             'first_name': 'name'
         }
+
+    
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+
+        # self.fields['title'].widget.attrs.update({'class':'input', 'placeholder':'Add title'})
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class':'input'})
