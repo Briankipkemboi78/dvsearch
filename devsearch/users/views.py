c36ae47c61fb2f0c8971e65d2c580f8049c8b65e
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-12-30 06:58:46 +0300

    Improving authentications

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -1,17 +1,37 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
 from .models import Profile
 
 
-def loginPage(request):
+def loginUser(request):
+
+    if request.user.is_authenticated:
+        return redirect('profiles')
 
     if request.method == 'POST':
-        username = request.POST['user']
+        username = request.POST['username']
+        password = request.POST['password']
 
         try:
             user = User.objects.get(username=username)
+        except:
+            print("Username does not exist")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('profiles')
+        else:
+            print('Username OR Password is incorrect')
+
     return render(request, 'users/login_register.html')
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
 
 def profiles(request):
     profiles = Profile.objects.all()
